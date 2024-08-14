7b2cf7d630dad097892f9bba05218eb4701fd654
Vedant Yaduvanshi-) <87130565+vedantyaduvanshi@users.noreply.github.com>
2024-08-14 15:35:46 +0530

    Update Loader.jsx

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -4,28 +4,19 @@ import { gsap } from 'gsap';
 
 const Loader = () => {
   useEffect(() => {
-    const svg = document.getElementById('svg');
     const tl = gsap.timeline();
-    const curve = 'M0 502S175 272 500 272s500 230 500 230V0H0Z';
-    const flat = 'M0 2S175 1 500 1s500 1 500 1V0H0Z';
 
     tl.to(`.${styles.loadText}`, {
       delay: 1.5,
       y: -100,
       opacity: 0,
     });
-    tl.to(svg, {
-      duration: 0.5,
-      attr: { d: curve },
-      ease: 'power2.easeIn',
-    }).to(svg, {
-      duration: 0.5,
-      attr: { d: flat },
-      ease: 'power2.easeOut',
-    });
+
     tl.to(`.${styles.loaderWrap}`, {
+      duration: 1.5, // Increase the duration to slow down the slide-up animation
       y: -1500,
     });
+
     tl.to(`.${styles.loaderWrap}`, {
       zIndex: -1,
       display: 'none',
@@ -35,7 +26,8 @@ const Loader = () => {
   return (
     <div className={styles.loaderWrap}>
       <svg viewBox="0 0 1000 1000" preserveAspectRatio="none">
-        <path id="svg" d="M0,1005S175,995,500,995s500,5,500,5V0H0Z"></path>
+        {/* Full-height flat background */}
+        <path id="svg" d="M0,1000H1000V0H0Z"></path>
       </svg>
       <div className={styles.loaderWrapHeading}>
         <div className={styles.loadText}>
